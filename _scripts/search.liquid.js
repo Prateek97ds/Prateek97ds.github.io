0aad87473eb9d1d56fb43a514a3f8f5b223e9427
PRATEEK TIWARI <77778406+Prateek97ds@users.noreply.github.com>
2025-12-18 18:59:36 +0530

    Update search.liquid.js

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -6,9 +6,16 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [
+  // {%- for page in site.pages -%}
+  //   {%- if page.permalink == '/' -%}{%- assign about_title = page.title | strip -%}{%- endif -%}
+  // {%- endfor -%}
+  {%- assign about_title = "About" -%}
   {%- for page in site.pages -%}
-    {%- if page.permalink == '/' -%}{%- assign about_title = page.title | strip -%}{%- endif -%}
+    {%- if page.permalink == '/' and page.title -%}
+      {%- assign about_title = page.title | strip -%}
+    {%- endif -%}
   {%- endfor -%}
+
   {
     id: "nav-{{ about_title | slugify }}",
     title: "{{ about_title | truncatewords: 13 }}",
