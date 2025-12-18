dafbe70b96514d54adca3ea0f1be26e0ca804a33
Prateek97ds <77778406+Prateek97ds@users.noreply.github.com>
2025-12-18 09:25:38 +0000

    Deploying to gh-pages from @ Prateek97ds/Prateek97ds.github.io@100819d6614633ecfc6a525d03e758b79b0c3c04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-about",
+          title: "About",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/about/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "Research projects on AI, skills, labor markets, and data infrastructure.",
           section: "Navigation",
